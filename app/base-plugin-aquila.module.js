c05d23649f29a05ed6532b499fcd1b7412fda4f7
n4n5 <56606507+Its-Just-Nans@users.noreply.github.com>
2020-11-16 16:13:16 +0100

    [fix] name

--- app/base-plugin-aquila.module.js
+++ app/base-plugin-aquila.module.js
@@ -3,13 +3,17 @@ var basePlugin = {name: 'aq.base-plugin-aquila'};
 angular.module(`${basePlugin.name}`, [
     `${basePlugin.name}.controllers`,
     `${basePlugin.name}.routes`,
-    `${basePlugin.name}.services`
+    `${basePlugin.name}.services`//,
+    //`${basePlugin.name}.directives`
 ]);
+// uncommented to use a decorator note that you need to have setup the service
 
+/*
 angular.module('aq.dependencies').requires.push(`${basePlugin.name}`);
 
+
 angular.module('adminCatagenApp').config(['$provide', function ($provide) {
-    $provide.decorator('HookPage', [
+    $provide.decorator('HookPageProduct', [
         '$delegate',
         function myServiceDecorator($delegate) {
             $delegate = $delegate.concat([
@@ -25,3 +29,5 @@ angular.module('adminCatagenApp').config(['$provide', function ($provide) {
         }
     ]);
 }]);
+
+*/
